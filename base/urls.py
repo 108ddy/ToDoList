7117680854a7bf858ced7b20be1debfec65d931e
108ddy <mgbemena900@gmail.com>
2022-08-12 00:46:37 +0300

    Implemented CRUD operations.

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 
 app_name = 'base'
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+
+    path('create/', TaskCreate.as_view(), name='create'),
+    path('update/<int:pk>/', TaskUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', TaskDelete.as_view(), name='delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Task
 
 
@@ -12,3 +15,37 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = 'base/task_create.html'
+    fields = ['title', 'description']
+    success_url = reverse_lazy('base:tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        return super(TaskCreate, self).form_valid(form)
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    template_name = 'base/task_update.html'
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('base:task')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        return super(TaskUpdate, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('base:task', kwargs={'pk': self.object.pk})
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    template_name = 'base/task_delete.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('base:tasks')
