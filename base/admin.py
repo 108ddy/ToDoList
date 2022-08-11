bf295ee8c81ccec528b78570982983fade629337
108ddy <mgbemena900@gmail.com>
2022-08-11 15:48:05 +0300

    Implemented display of Task model in admin site.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'description', 'complete', 'created',)
+    search_fields = ('user', 'title',)
+    list_filter = ('complete', 'created',)
+
+
+admin.site.register(Task, TaskAdmin)
